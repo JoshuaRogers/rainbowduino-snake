4dea0d028c3a210c94dcf53c9cc2b23a5ae8c900
Joshua Rogers <git@joshuarogers.net>
2015-10-26 14:03:18 -0500

    Lowers requirement for winning to 20.

--- winrule.cpp
+++ winrule.cpp
@@ -3,7 +3,7 @@
 
 void WinRule::execute(Game* game)
 {
-    if (game->snake.length() >= 32 && game->state == Game::Running) {
+    if (game->snake.length() >= 20 && game->state == Game::Running) {
         game->state = Game::Win;
     }
 }
